774350c447e6884a6cdaebd9ad6c13fe93422c42
Doc <65479309+DocilElm@users.noreply.github.com>
2024-05-24 12:44:14 -0700

    separate Button1 from Button scheme & add more customizability to it

--- elements/Button1.js
+++ elements/Button1.js
@@ -1,10 +1,10 @@
-import { Animations, CenterConstraint, ConstantColorConstraint, UIRoundedRectangle, animate } from "../../Elementa"
+import { Animations, CenterConstraint, ConstantColorConstraint, OutlineEffect, UIRoundedRectangle, animate } from "../../Elementa"
 import BaseElement from "./Base"
 import TextElement from "./Text"
 
 export default class Button1Element extends BaseElement {
     constructor(string = "Placeholder", x, y, width, height) {
-        super(x, y, width, height, null, null, "Button")
+        super(x, y, width, height, null, null, "Button1")
 
         this.string = string
     }
@@ -13,40 +13,44 @@ export default class Button1Element extends BaseElement {
         if (!this.colorScheme) this.colorScheme = colorScheme
 
         // Invis box to hold everything
-        this.backgroundBox = new UIRoundedRectangle(3)
+        this.backgroundBox = new UIRoundedRectangle(5)
             .setX(this.x)
             .setY(this.y)
             .setWidth(this.width)
             .setHeight(this.height)
-            .setColor(this._getColor("backgroundBox1"))
+            .setColor(this._getColor("backgroundBox"))
+            .enableEffect(new OutlineEffect(this._getColor("outlineColor"), this._getSchemeValue("outlineThickness")))
 
-        // Vertical line
-        this.line1 = new UIRoundedRectangle(3)
-            .setX((1).pixel())
-            .setY(new CenterConstraint())
-            .setWidth((2).pixel())
-            .setHeight((10).pixel())
-            .setColor(this._getColor("lines"))
-            .setChildOf(this.backgroundBox)
-    
-        // Horizontal line
-        this.line2 = new UIRoundedRectangle(3)
-            .setX((2).pixel())
-            .setY((60).percent())
-            .setWidth((80).percent())
-            .setHeight((2).pixel())
-            .setColor(this._getColor("lines"))
-            .setChildOf(this.backgroundBox)
+        // If [centered] text is disabled we should draw the lines
+        if (!this._getSchemeValue("centered")) {
+            // Vertical line
+            this.line1 = new UIRoundedRectangle(3)
+                .setX((1).percent())
+                .setY(new CenterConstraint())
+                .setWidth((2).percent())
+                .setHeight((50).percent())
+                .setColor(this._getColor("lines"))
+                .setChildOf(this.backgroundBox)
+            
+            // Horizontal line
+            this.line2 = new UIRoundedRectangle(3)
+                .setX((1).percent())
+                .setY((70).percent())
+                .setWidth((80).percent())
+                .setHeight((8).percent())
+                .setColor(this._getColor("lines"))
+                .setChildOf(this.backgroundBox)
+        }
 
         // Centered to lines text
         const [ x, y, width, height ] = [
-            (5).pixel(),
+            (8).percent(),
             (20).percent(),
             (80).percent(),
             this.height
         ]
 
-        this.text = new TextElement(this.getString())
+        this.text = new TextElement(this.getString(), this._getSchemeValue("centered"))
             ._setPosition(x, y)
             ._setSize(width, height)
             ._create(this.colorScheme)
@@ -59,8 +63,8 @@ export default class Button1Element extends BaseElement {
             
                 animate(comp, (animation) => {
                     animation.setColorAnimation(
-                        Animations[this._getSchemeValue("mouseClickAnimation")],
-                        this._getSchemeValue("animationTime"),
+                        Animations[this._getSchemeValue("mouseEnterAnimation")],
+                        this._getSchemeValue("mouseEnterAnimationTime"),
                         new ConstantColorConstraint(this._getColor("mouseEnter")),
                         0
                         )
@@ -71,8 +75,8 @@ export default class Button1Element extends BaseElement {
             
                 animate(comp, (animation) => {
                     animation.setColorAnimation(
-                        Animations[this._getSchemeValue("mouseClickAnimation")],
-                        this._getSchemeValue("animationTime"),
+                        Animations[this._getSchemeValue("mouseLeaveAnimation")],
+                        this._getSchemeValue("mouseLeaveAnimationTime"),
                         new ConstantColorConstraint(this._getColor("mouseLeave")),
                         0
                         )
@@ -84,13 +88,13 @@ export default class Button1Element extends BaseElement {
                 animate(comp, (animation) => {
                     animation.setColorAnimation(
                         Animations[this._getSchemeValue("mouseClickAnimation")],
-                        this._getSchemeValue("animationTime"),
+                        this._getSchemeValue("mouseClickAnimationTime"),
                         new ConstantColorConstraint(this._getColor("mouseClick")),
                         0
                         )
                     
                     animation.onComplete(() => {
-                        comp.setColor(this._getColor("backgroundBox1"))
+                        comp.setColor(this._getColor("backgroundBox"))
                     })
                 })
             })
