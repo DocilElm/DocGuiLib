20c222a3de9c1ab06eb7cb5ffa9c71b27a6637a9
Doc <65479309+DocilElm@users.noreply.github.com>
2024-09-30 12:08:24 -0700

    change TextDescription to properly adjust the text

--- elements/TextDescription.js
+++ elements/TextDescription.js
@@ -1,4 +1,5 @@
-import { UIWrappedText } from "../../Elementa"
+import { CramSiblingConstraint, UIBlock, UIWrappedText } from "../../Elementa"
+import ElementUtils from "../core/Element"
 import BaseElement from "./Base"
 
 export default class TextDescriptionElement extends BaseElement {
@@ -13,23 +14,30 @@ export default class TextDescriptionElement extends BaseElement {
         if (!this.colorScheme) this.setColorScheme(colorScheme)
         if (elementType) this.elementType = elementType
 
-        this.text = new UIWrappedText(this.getValue(), true, null, this.centered, true, 10, "...")
+        this.bgBox = new UIBlock(ElementUtils.getJavaColor([0, 0, 0, 0]))
             .setX(this.x)
             .setY(this.y)
             .setWidth(this.width)
             .setHeight(this.height)
+
+        this.text = new UIWrappedText(this.getValue(), true, null, this.centered, true, 10, "...")
+            .setX((0).pixels())
+            .setY((0).pixels())
+            .setWidth((98).percent())
+            .setHeight((30).percent())
             .setTextScale((this._getSchemeValue("text", "scale")).pixels())
             .setColor(this._getColor("text", "color"))
+            .setChildOf(this.bgBox)
 
         this.descriptionElement = new UIWrappedText(this.description, true, null, this.centered, true, 10, "...")
-            .setX(this.x)
-            .setY((11).pixel())
-            .setWidth(this.width)
-            .setHeight(this.height)
+            .setX((0).percent())
+            .setY(new CramSiblingConstraint(2))
+            .setWidth((98).percent())
+            .setHeight((75).percent())
             .setTextScale((this._getSchemeValue("text", "scale")).pixels())
             .setColor(this._getColor("text", "color").darker())
-            .setChildOf(this.text)
+            .setChildOf(this.bgBox)
 
-        return this.text
+        return this.bgBox
     }
 }
