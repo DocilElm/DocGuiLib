9f6f5703180ab16051d3ea5411c4a78f2e42ec87
Doc <65479309+DocilElm@users.noreply.github.com>
2025-02-08 13:24:56 -0700

    TextDescription: add `setWrapHeight` function

--- elements/TextDescription.js
+++ elements/TextDescription.js
@@ -8,6 +8,18 @@ export default class TextDescriptionElement extends BaseElement {
 
         this.centered = centered
         this.description = description
+        this.wrapHeight = true
+    }
+
+    /**
+     * - Whether to wrap the text's height or not
+     * @param {boolean} bool 
+     * @returns this for method chaining
+     */
+    setWrapHeight(bool) {
+        this.wrapHeight = bool
+
+        return this
     }
 
     _create(colorScheme = {}, elementType = null) {
@@ -24,7 +36,6 @@ export default class TextDescriptionElement extends BaseElement {
             .setX((0).pixels())
             .setY((0).pixels())
             .setWidth((98).percent())
-            .setHeight((30).percent())
             .setTextScale((this._getSchemeValue("text", "scale")).pixels())
             .setColor(this._getColor("text", "color"))
             .setChildOf(this.bgBox)
@@ -33,10 +44,14 @@ export default class TextDescriptionElement extends BaseElement {
             .setX((0).percent())
             .setY(new CramSiblingConstraint(2))
             .setWidth((98).percent())
-            .setHeight((75).percent())
             .setTextScale((this._getSchemeValue("text", "scale")).pixels())
             .setColor(this._getColor("text", "color").darker())
             .setChildOf(this.bgBox)
+
+        if (this.wrapHeight) {
+            this.text.setHeight((30).percent())
+            this.descriptionElement.setHeight((75).percent())
+        }
 
         return this.bgBox
     }
